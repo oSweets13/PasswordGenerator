4e5167abc9b137610956bdcc03615fb1a8fe0309
oSweets13 <olivia13perkins@gmail.com>
2023-07-05 10:18:59 -0600

    added comments to sections of the code

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//Password Characters
+//Password Character Inputs
 var inputs = [""]
 var uppercases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var lowercases = "abcdefghijklmnopqrstuvwxyz"
 var numbers = "0123456789"
 var special = "!#$%&*+-./:;=?@"
 var randomPassword = ""
-
 
 
 // Write password to the #password input
@@ -23,6 +22,8 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+//Write function to generate the random password
 function generatePassword () {
   var passwordLength = prompt("How many characters would you like?");
     if (passwordLength < 8 || passwordLength > 182 || isNaN(passwordLength)) {
